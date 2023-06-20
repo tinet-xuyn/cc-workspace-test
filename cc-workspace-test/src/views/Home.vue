06d5484fd86283011d16865608689105ab29bd78
xuyn <xuyn@ti-net.com.cn>
2023-06-20 14:00:31 +0800

    init

---
+++ cc-workspace-test/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+      <div v-for="item in options" :key="item.id">
+        {{ item.key }}: {{ item.value }}
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from 'vue';
+import { Vue } from 'vue-class-component';
+export default class Home extends Vue {
+ options: any = reactive([]);
+
+  created() {
+    const searchParams: any = new URLSearchParams(location.search);
+    for(var key of searchParams.keys()) {
+      this.options.push({
+        key,
+        value: searchParams.get(key)
+      })
+    }
+  }
+}
+</script>
